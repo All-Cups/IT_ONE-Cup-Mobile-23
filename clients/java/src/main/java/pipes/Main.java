89580687a5ac0d02d873f36b2b2d379d5bbbe22a
Nirenamid <18709286+Nirenamid@users.noreply.github.com>
2023-04-07 16:43:16 +0300

    Контейнеры

---
+++ clients/java/src/main/java/pipes/Main.java
@@ -0,0 +1,8 @@
+package pipes;
+
+public class Main {
+    public static void main(String[] args) {        
+        
+        System.out.println("Java");
+    }
+}
---
+++ clients/swift/Sources/Pipes/main.swift
@@ -0,0 +1,9 @@
+import Foundation
+#if canImport(FoundationNetworking)
+    import FoundationNetworking
+#endif
+
+// let url = URL(string: "http://localhost")!
+// var request = URLRequest(url: url)
+
+print("Swift")
